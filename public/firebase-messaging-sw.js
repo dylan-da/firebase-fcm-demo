e695c39f3fc4a62f7b36b4b180946b4337b1816e
Dylan Knevitt <dylan@thedigitalacademy.co.za>
2024-12-11 15:06:24 +0200

    added html

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,5 +1,5 @@
-importScripts("https://www.gstatic.com/firebasejs/11.0.2/firebase-app-compat.js");
-importScripts("https://www.gstatic.com/firebasejs/11.0.2/firebase-messaging-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/11.0.2/firebase-app.js");
+importScripts("https://www.gstatic.com/firebasejs/11.0.2/firebase-messaging.js");
 
 const firebaseConfig = {
     apiKey: "AIzaSyBUBlA8mZAetHrflR8NQUI00K0tFZ-G3Ro",
